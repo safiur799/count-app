03370abadca756c8ec950d574a4a1ac472537302
safiur799 <safiurrahaman799@gmail.com>
2022-01-25 15:33:33 +0530

    add reducer functionality

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -1,12 +1,16 @@
 import React from "react";
-
+import { useState } from "react";
 function Counter() {
   const [count, setCount] = useState(0);
+
+  const clickHandler = (num) => {
+    setCount(count + num);
+  };
   return (
     <div>
       <h1>{count}</h1>
-      <button>add</button>
-      <button>Reduce</button>
+      <button onClick={() => clickHandler(1)}>add</button>
+      <button onClick={() => clickHandler(-1)}>Reduce</button>
     </div>
   );
 }
